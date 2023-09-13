96f065695d16604271763293a2ce44522dda04e3
Arie <arie@arie.software>
2023-09-13 20:40:27 +0300

    cleanup

--- store/actions.ts
+++ store/actions.ts
@@ -6,7 +6,6 @@ import axios from "axios";
 export const fetchMoviesAtom = atom(null, async (get, set) => {
   try {
     const response = await axios.get("/api/movies");
-    console.log("response.data.results:", response.data.results);
     const movies: Movie[] = response.data.results;
     set(moviesAtom, movies);
   } catch (error) {

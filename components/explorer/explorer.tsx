cee0c1e5dd8af9f23f8b27c01a198e7b8ed00e91
Arie <arie@arie.software>
2023-09-13 20:40:10 +0300

    jotai data

--- components/explorer/explorer.tsx
+++ components/explorer/explorer.tsx
@@ -1,10 +1,14 @@
 import { Grid, Paper, Select, TextField } from "@mui/material";
-import { Pane } from "./pane";
 import styles from "./explorer.module.css";
+import { moviesAtom } from "../../store/atoms";
+import { useAtom } from "jotai";
+import { Pane } from "./pane";
 
 interface Props {}
 
 export const Explorer = (props: Props) => {
+  const [movies] = useAtom(moviesAtom);
+
   return (
     <Paper square className={styles.explorer_container}>
       <Grid container columns={4}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
 import Layout from "../components/layout";
 import { Box } from "@mui/material";
 import { Explorer } from "../components/explorer/explorer";
+import { fetchMoviesAtom } from "../store/actions";
+import { useAtom } from "jotai";
+import { useEffect } from "react";
 
 export default function Home({}) {
+  const [, fetchMovies] = useAtom(fetchMoviesAtom);
+
+  useEffect(() => {
+    fetchMovies().catch(console.error);
+  }, []);
+
   return (
     <Layout>
       <Box sx={{ height: "100%", width: "100%" }}>

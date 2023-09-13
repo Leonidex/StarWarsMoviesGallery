5822d07623fa51c841c2a9b289e1d3e7c130e7b6
Arie <arie@arie.software>
2023-09-13 20:32:02 +0300

    movie type

---
+++ types/index.ts
@@ -0,0 +1,17 @@
+export interface Movie {
+  id: number;
+  title: string;
+  characters: string[];
+  created: Date;
+  director: string;
+  edited: Date;
+  episode_id: number;
+  opening_crawl: string;
+  planets: string[];
+  producer: string;
+  release_date: Date;
+  species: string[];
+  starships: string[];
+  url: string;
+  vehicles: string[];
+}

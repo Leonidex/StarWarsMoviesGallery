1cbdfa012c2f77b7f76303dd2d23e67c9cd26bc9
Arie <arie@arie.software>
2023-09-18 16:55:53 +0300

    ref fix

--- components/explorer/error-modal.tsx
+++ components/explorer/error-modal.tsx
@@ -1,4 +1,4 @@
-import { Modal, Typography } from "@mui/material";
+import { Box, Modal, Typography } from "@mui/material";
 import { Center } from "../basic/center";
 
 interface Props {
@@ -8,11 +8,13 @@ interface Props {
 export const ErrorModal = (props: Props) => {
   return (
     <Modal open={props.isError}>
-      <Center>
-        <Typography color={"error"}>
-          A network error has occurred, please refresh the page to try again.
-        </Typography>
-      </Center>
+      <Box>
+        <Center>
+          <Typography color={"error"}>
+            A network error has occurred, please refresh the page to try again.
+          </Typography>
+        </Center>
+      </Box>
     </Modal>
   );
 };

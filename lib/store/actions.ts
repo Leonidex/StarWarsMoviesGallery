9dbd47d028e3d3b5b25c72701aab397922241c0f
Arie <arie@arie.software>
2023-09-18 15:10:30 +0300

    slow API fix...

--- lib/store/actions.ts
+++ lib/store/actions.ts
@@ -1,4 +1,4 @@
-import { atom } from "jotai";
+import { atom, Setter } from "jotai";
 import { Film } from "../types";
 import { filmsAtom, loadingSideAtom, selectedFilmAtom, Side } from "./atoms";
 import axios from "axios";
@@ -6,12 +6,28 @@ import axios from "axios";
 export const fetchFilmsAtom = atom(null, async (get, set) => {
   try {
     const response = await axios.get("/api/films");
+
     const films: Film[] = response.data;
     set(filmsAtom, films);
   } catch (error) {
-    console.error("There was an error fetching the films", error);
+    await clientFetchFilms(set);
+    console.debug("There was an error fetching the films using API", error);
   }
 });
+
+// Using this because the API is very slow and Vercel allows only 10 seconds until the request times-out
+const clientFetchFilms = async (set: Setter) => {
+  try {
+    const response = await axios.get(
+      `https://swapi.dev/api/films/?format=json`,
+    );
+
+    const films: Film[] = response?.data?.results;
+    set(filmsAtom, films);
+  } catch (error) {
+    console.debug("There was an error fetching the films using client", error);
+  }
+};
 
 export const setSelectedFilmAtom = atom(null, (get, set, film: Film) => {
   const currentlySelectedFilm = get(selectedFilmAtom);
